b1925c7b6871af69e7ccf21824b19d694cd814be
tranquangtanqt <tranquangtanqt1990@gmail.com>
2018-12-20 19:00:11 +0700

    Trang 30 - Bài 2 - Xâu con Plindrome 2

---
+++ BaiTap/T30_Bai1_XauPalindrome2/T30_Bai1_XauPalindrome2.pas
@@ -0,0 +1,72 @@
+{ Cho xâu S có độ dài không vượt quá 1000 ký tự. Tìm xâu palindrome con dài nhất là xâu con của S }
+program T30_Bai1_XauPalindrome2;
+var
+	s: String;
+
+{ Nhap xau s }
+procedure Nhap();
+begin
+	writeln('Nhap xau s: '); readln(s);
+end;
+
+{ Tim xau con dai nhat }
+function XauConDaiNhat(): String;
+var
+	truoc, sau, max, start, i: Integer;
+	kq: String;
+begin
+	//Truong hop xau s rong
+	if length(s) = 0 then 
+	begin 
+		XauConDaiNhat := ''; 
+		exit(); 
+	end;
+
+	//Truong hop xau s chi gom 1 ky tu
+	if length(s) = 1 then
+	begin
+		XauConDaiNhat := s; 
+		exit(); 
+	end;
+
+	//Truong hop xau s co do dai >= 2
+	max := 1;
+	for i := 2 to length(s) do
+	begin
+		//Xet xau con co so luong chan
+		truoc := i - 1;
+		sau := i;
+		while (truoc >= 1) and (sau <= length(s)) and (s[truoc] = s[sau]) do
+		begin
+			if sau - truoc + 1 > max then
+			begin
+				start := truoc;
+				max := sau - truoc + 1;
+			end;
+			dec(truoc);
+			inc(sau);
+		end;
+
+		//Xet xau con co so luong le (tam tai s[i])
+		truoc := i - 1;
+		sau := i + 1;
+		while (truoc >= 1) and (sau <= length(s)) and (s[truoc] = s[sau]) do
+		begin
+			if sau - truoc + 1 > max then
+			begin
+				start := truoc;
+				max := sau - truoc + 1;
+			end;
+			dec(truoc);
+			inc(sau);
+		end;
+	end;
+
+	XauConDaiNhat := copy(s, start, max);
+end;
+
+BEGIN
+	Nhap();
+	writeln('Xau con dai nhat: ', XauConDaiNhat());
+	readln
+END.
