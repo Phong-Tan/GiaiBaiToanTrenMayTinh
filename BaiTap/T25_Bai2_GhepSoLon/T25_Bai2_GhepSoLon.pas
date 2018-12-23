ac5dcf50b6281a8488fcfb27d6eaa7ec7c8e0465
tranquangtanqt <tranquangtanqt1990@gmail.com>
2018-12-23 18:44:11 +0700

    Sua T25_Bai2_GhepSoLon

--- BaiTap/T25_Bai2_GhepSoLon/T25_Bai2_GhepSoLon.pas
+++ BaiTap/T25_Bai2_GhepSoLon/T25_Bai2_GhepSoLon.pas
@@ -1,9 +1,6 @@
-{Nhập vào một hoặc nhiều dòng. Mỗi dòng ghi một dãy ký tự. Số dòng không vượt quá 100. Mỗi dòng ghi từ 1 đến 100 ký tự số. 
-Bảo đảm rằng có ít nhất một dòng mà ký tự số đầu tiên khác 0. Ghi ra màn hình số lớn nhất có thể có khi ghép toàn bộ 
-các số vừa mới nhập vào.}
 program T25_Bai2_GhepSoLon;
 var
-	S: array[1..1000] of String;
+	S: array[0..1000] of String;
 	n: Integer;
 
 { Nhap du lieu }
@@ -22,15 +19,16 @@ end;
 phan tu s[i] dung truoc phan tu s[j] neu s[i] ghep voi s[j] > s[j] ghep voi s[i]}
 procedure SapXep();
 var
-	i, j, a, b, code: Integer;
-	temp: String;
+	i, j: Integer;
+	temp, s1, s2: String;
 begin
 	for i := 1 to n - 1 do
 		for j := i + 1 to n do
 		begin
-			val(s[i] + s[j], a, code);
-			val(s[j] + s[i], b, code);
-			if a < b then
+			s1 := s[i]; s2 := s[j];
+			while length(s1) < length(s2) do s1 += '0';
+			while length(s2) < length(s1) do s2 += '0';
+			if s1 < s2 then
 			begin
 				temp := s[i]; s[i] := s[j]; s[j] := temp;
 			end;
